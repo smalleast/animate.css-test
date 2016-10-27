f4f63f1de58d4377289abd669e099af523342b8b
tony <huangxiaodong@dcpai.cn>
2016-10-27 18:00:55 +0800

    add animate.css-test

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+/**
+ *  Welcome to your gulpfile!
+ *  The gulp tasks are splitted in several files in the gulp directory
+ *  because putting all here was really too long
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'gulp.*', 'del']
+});
+
+
+gulp.task('clean', function () {
+  return del('dist');
+});
+gulp.task('styles', function () {
+  return gulp.src('bower_components/animate.css/**.css')
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('htmls', function() {
+  return gulp.src(['src/**/*.html'])
+    .pipe($.fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe($.htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['clean'], function () {
+  gulp.start(['styles','htmls']);
+});
